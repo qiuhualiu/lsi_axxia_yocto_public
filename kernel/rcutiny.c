7a9fc4208497ca1da217fdea2495743856347a7f
Bruce Ashfield <bruce.ashfield@windriver.com>
2012-07-01 00:16:55 -0400

    Revert "preempt-rt: import 3.4.1-rt9"
    This reverts commit 9dc5e97d144abb03deec411acb9c64883602cff9.

--- kernel/rcutiny.c
+++ kernel/rcutiny.c
@@ -368,7 +368,6 @@ void call_rcu_sched(struct rcu_head *head, void (*func)(struct rcu_head *rcu))
 }
 EXPORT_SYMBOL_GPL(call_rcu_sched);
 
-#ifndef CONFIG_PREEMPT_RT_FULL
 /*
  * Post an RCU bottom-half callback to be invoked after any subsequent
  * quiescent state.
@@ -378,4 +377,3 @@ void call_rcu_bh(struct rcu_head *head, void (*func)(struct rcu_head *rcu))
 	__call_rcu(head, func, &rcu_bh_ctrlblk);
 }
 EXPORT_SYMBOL_GPL(call_rcu_bh);
-#endif
