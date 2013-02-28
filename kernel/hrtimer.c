c3fa9782d3c6caabbca85d56fc4ace0ba97b9d9e
Bruce Ashfield <bruce.ashfield@windriver.com>
2013-02-28 15:55:25 -0500

    hrtimer: fix v3.4.34 -> rt merge
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- kernel/hrtimer.c
+++ kernel/hrtimer.c
@@ -1035,6 +1035,15 @@ int __hrtimer_start_range_ns(struct hrtimer *timer, ktime_t tim,
 	if (leftmost && new_base->cpu_base == &__get_cpu_var(hrtimer_bases)
 		&& hrtimer_enqueue_reprogram(timer, new_base)) {
 		if (wakeup) {
+#ifdef CONFIG_PREEMPT_RT_BASE
+			/*
+			 * Move softirq based timers away from the rbtree in
+			 * case it expired already. Otherwise we would have a
+			 * stale base->first entry until the softirq runs.
+			 */
+			if (!hrtimer_rt_defer(timer))
+				return -ETIME;
+#endif
 			/*
 			 * We need to drop cpu_base->lock to avoid a
 			 * lock ordering issue vs. rq->lock.
