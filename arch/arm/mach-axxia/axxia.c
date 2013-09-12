37bd9966ae55f0253fd610a8cb1e619d1e0892e4
Anders Berg <anders.berg@lsi.com>
2013-09-13 00:59:19 +0200

    New I2C bus driver
    Signed-off-by: Anders Berg <anders.berg@lsi.com>

--- arch/arm/mach-axxia/axxia.c
+++ arch/arm/mach-axxia/axxia.c
@@ -239,10 +239,6 @@ void __init axxia_dt_init(void)
 	}
 
 	/*axxia_pcie_init();*/
-
-#ifdef CONFIG_I2C
-	axxia_register_i2c_busses();
-#endif
 }
 
 static void axxia_restart(char str, const char *cmd)
