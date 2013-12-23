6495aa11dd2a682f2bee4ae9c2db8d94c7cd3617
John Jacques <john.jacques@lsi.com>
2013-12-23 14:08:36 -0600

    arch/arm/mach-axxia: Updated i2c for Simulation
    Simulation updates for i2c broke Simulation; this only applies
    the changes in 0b62b05ced2d04fb1a7707ff10169db580ba3eea on
    hardward.
    
    Signed-off-by: John Jacques <john.jacques@lsi.com>

--- drivers/i2c/busses/i2c-axxia.c
+++ drivers/i2c/busses/i2c-axxia.c
@@ -27,8 +27,12 @@
 #include <linux/module.h>
 
 #define SCL_WAIT_TIMEOUT_NS 25000000
+#ifdef CONFIG_ARCH_AXXIA_SIM
+#define I2C_TIMEOUT         (msecs_to_jiffies(1000))
+#else
 #define I2C_XFER_TIMEOUT    (msecs_to_jiffies(500))
 #define I2C_STOP_TIMEOUT    (msecs_to_jiffies(100))
+#endif
 #define TX_FIFO_SIZE        8
 #define RX_FIFO_SIZE        8
 
@@ -124,10 +128,19 @@ struct axxia_i2c_dev {
 	int irq;
 	/* xfer completion object */
 	struct completion msg_complete;
+#ifdef CONFIG_ARCH_AXXIA_SIM
+	/* pointer to current message data */
+	u8 *msg_buf;
+	/* size of unsent data in the message buffer */
+	size_t msg_buf_remaining;
+	/* identifies read transfers */
+	int msg_read;
+#else
 	/* pointer to current message */
 	struct i2c_msg *msg;
 	/* number of bytes transferred in msg */
 	size_t msg_xfrd;
+#endif
 	/* error code for completed message */
 	int msg_err;
 	/* current i2c bus clock rate */
@@ -171,10 +184,12 @@ axxia_i2c_init(struct axxia_i2c_dev *idev)
 	dev_dbg(idev->dev, "rate=%uHz per_clk=%uMHz -> ratio=1:%u\n",
 		idev->bus_clk_rate, clk_mhz, divisor);
 
+#ifndef CONFIG_ARCH_AXXIA_SIM
 	/* Reset controller */
 	writel(0x01, &idev->regs->soft_reset);
 	while (readl(&idev->regs->soft_reset) & 1)
 		cpu_relax();
+#endif
 
 	/* Enable Master Mode */
 	writel(0x1, &idev->regs->global_control);
@@ -190,8 +205,13 @@ axxia_i2c_init(struct axxia_i2c_dev *idev)
 
 	/* SDA Setup Time */
 	writel(t_setup, &idev->regs->sda_setup_time);
+#ifdef CONFIG_ARCH_AXXIA_SIM
+	/* SDA Hold Time, 5ns */
+	writel(ns_to_clk(5, clk_mhz), &idev->regs->sda_hold_time);
+#else
 	/* SDA Hold Time, 300ns */
 	writel(ns_to_clk(300, clk_mhz), &idev->regs->sda_hold_time);
+#endif
 	/* Filter <50ns spikes */
 	writel(ns_to_clk(50, clk_mhz), &idev->regs->spike_fltr_len);
 
@@ -232,7 +252,8 @@ axxia_i2c_init(struct axxia_i2c_dev *idev)
 	return 0;
 }
 
-static int
+#ifndef CONFIG_ARCH_AXXIA_SIM
+ static int
 i2c_m_rd(const struct i2c_msg *msg)
 {
 	return (msg->flags & I2C_M_RD) != 0;
@@ -243,14 +264,25 @@ i2c_m_recv_len(const struct i2c_msg *msg)
 {
 	return (msg->flags & I2C_M_RECV_LEN) != 0;
 }
+#endif
 
 static int
 axxia_i2c_empty_rx_fifo(struct axxia_i2c_dev *idev)
 {
-	struct i2c_msg *msg = idev->msg;
 	size_t rx_fifo_avail = readl(&idev->regs->mst_rx_fifo);
+#ifdef CONFIG_ARCH_AXXIA_SIM
+	int bytes_to_transfer = min(rx_fifo_avail, idev->msg_buf_remaining);
+#else
+	struct i2c_msg *msg = idev->msg;
 	int bytes_to_transfer = min(rx_fifo_avail, msg->len - idev->msg_xfrd);
+#endif
 
+#ifdef CONFIG_ARCH_AXXIA_SIM
+	idev->msg_buf_remaining -= bytes_to_transfer;
+
+	while (0 < bytes_to_transfer--)
+		*idev->msg_buf++ = readl(&idev->regs->mst_data);
+#else
 	while (0 < bytes_to_transfer--) {
 		int c = readl(&idev->regs->mst_data);
 		if (idev->msg_xfrd == 0 && i2c_m_recv_len(msg)) {
@@ -267,6 +299,7 @@ axxia_i2c_empty_rx_fifo(struct axxia_i2c_dev *idev)
 		}
 		msg->buf[idev->msg_xfrd++] = c;
 	}
+#endif
 
 	return 0;
 }
@@ -274,16 +307,28 @@ axxia_i2c_empty_rx_fifo(struct axxia_i2c_dev *idev)
 static int
 axxia_i2c_fill_tx_fifo(struct axxia_i2c_dev *idev)
 {
-	struct i2c_msg *msg = idev->msg;
 	size_t tx_fifo_avail = TX_FIFO_SIZE - readl(&idev->regs->mst_tx_fifo);
+#ifdef CONFIG_ARCH_AXXIA_SIM
+	int bytes_to_transfer = min(tx_fifo_avail, idev->msg_buf_remaining);
+#else
+	struct i2c_msg *msg = idev->msg;
 	int bytes_to_transfer = min(tx_fifo_avail, msg->len - idev->msg_xfrd);
+#endif
+
+#ifdef CONFIG_ARCH_AXXIA_SIM
+	idev->msg_buf_remaining -= bytes_to_transfer;
 
 	while (0 < bytes_to_transfer--)
+		writel(*idev->msg_buf++, &idev->regs->mst_data);
+#else
+	while (0 < bytes_to_transfer--)
 		writel(msg->buf[idev->msg_xfrd++], &idev->regs->mst_data);
+#endif
 
 	return 0;
 }
 
+#if !defined(CONFIG_ARCH_AXXIA_SIM) || defined(DEBUG)
 static char *
 status_str(u32 status)
 {
@@ -311,6 +356,7 @@ status_str(u32 status)
 		strcat(buf, "NA ");
 	return buf;
 }
+#endif
 
 static irqreturn_t
 axxia_i2c_isr(int irq, void *_dev)
@@ -334,37 +380,158 @@ axxia_i2c_isr(int irq, void *_dev)
 		return IRQ_HANDLED;
 	}
 
+#ifndef CONFIG_ARCH_AXXIA_SIM
 	/* Stop completed? */
 	if (status & MST_STATUS_SCC) {
 		i2c_int_disable(idev, ~0);
 		complete(&idev->msg_complete);
 	}
+#endif
 
 	/* Transfer done? */
 	if (status & (MST_STATUS_SNS | MST_STATUS_SS)) {
-		if (i2c_m_rd(idev->msg) && idev->msg_xfrd < idev->msg->len)
+#ifdef CONFIG_ARCH_AXXIA_SIM
+		if (idev->msg_read && idev->msg_buf_remaining > 0)
 			axxia_i2c_empty_rx_fifo(idev);
+		WARN_ON(idev->msg_buf_remaining > 0);
+#else
+		if (i2c_m_rd(idev->msg) && idev->msg_xfrd < idev->msg->len)
+ 			axxia_i2c_empty_rx_fifo(idev);
+#endif
 		i2c_int_disable(idev, ~0);
 		complete(&idev->msg_complete);
 	}
 
 	/* RX FIFO needs service? */
-	if (i2c_m_rd(idev->msg) && (status & MST_STATUS_RFL)) {
+#ifdef CONFIG_ARCH_AXXIA_SIM
+	if (idev->msg_read && (status & MST_STATUS_RFL)) {
+		WARN_ON(idev->msg_buf_remaining == 0);
 		axxia_i2c_empty_rx_fifo(idev);
 	}
+#else
+	if (i2c_m_rd(idev->msg) && (status & MST_STATUS_RFL))
+ 		axxia_i2c_empty_rx_fifo(idev);
+#endif
 
 	/* TX FIFO needs service? */
+#ifdef CONFIG_ARCH_AXXIA_SIM
+	if (!idev->msg_read && (status & MST_STATUS_TFL)) {
+		if (idev->msg_buf_remaining)
+			axxia_i2c_fill_tx_fifo(idev);
+		else
+			i2c_int_disable(idev, MST_STATUS_TFL);
+	}
+#else
 	if (!i2c_m_rd(idev->msg) && (status & MST_STATUS_TFL)) {
 		if (idev->msg_xfrd < idev->msg->len)
 			axxia_i2c_fill_tx_fifo(idev);
 		else
 			i2c_int_disable(idev, MST_STATUS_TFL);
 	}
+#endif
 
 	return IRQ_HANDLED;
 }
 
+#ifdef CONFIG_ARCH_AXXIA_SIM
+static int
+axxia_i2c_xfer_msg(struct axxia_i2c_dev *idev, struct i2c_msg *msg, int stop)
+{
+	u32 int_mask;
+	int ret;
 
+	dev_dbg(idev->dev, "xfer_msg: chip=%#x, buffer=[%02x %02x %02x %02x], len=%d, stop=%d\n",
+		msg->addr, msg->buf[0], msg->buf[1], msg->buf[2], msg->buf[3],
+		msg->len, stop);
+
+	if (msg->len == 0 || msg->len > 255)
+		return -EINVAL;
+
+	idev->msg_buf           = msg->buf;
+	idev->msg_buf_remaining = msg->len;
+	idev->msg_err           = 0;
+	idev->msg_read          = (msg->flags & I2C_M_RD);
+	INIT_COMPLETION(idev->msg_complete);
+
+	if (msg->flags & I2C_M_RD) {
+		/* TX 0 bytes */
+		writel(0, &idev->regs->mst_tx_xfer);
+		/* RX # bytes */
+		writel(msg->len, &idev->regs->mst_rx_xfer);
+		/* Chip address for write */
+		writel(CHIP_READ(msg->addr & 0xfe), &idev->regs->mst_addr_1);
+	} else {
+		/* TX # bytes */
+		writel(msg->len, &idev->regs->mst_tx_xfer);
+		/* RX 0 bytes */
+		writel(0, &idev->regs->mst_rx_xfer);
+		/* Chip address for write */
+		writel(CHIP_WRITE(msg->addr & 0xfe), &idev->regs->mst_addr_1);
+	}
+	writel(msg->addr >> 8, &idev->regs->mst_addr_2);
+
+	if (!(msg->flags & I2C_M_RD))
+		axxia_i2c_fill_tx_fifo(idev);
+
+	int_mask = MST_STATUS_ERR;
+	int_mask |= stop ? MST_STATUS_SS : MST_STATUS_SNS;
+	if (msg->flags & I2C_M_RD)
+		int_mask |= MST_STATUS_RFL;
+	else if (idev->msg_buf_remaining)
+		int_mask |= MST_STATUS_TFL;
+
+	/* Start manual mode */
+	writel(stop ? 0x9 : 0x8, &idev->regs->mst_command);
+
+	i2c_int_enable(idev, int_mask);
+
+	ret = wait_for_completion_timeout(&idev->msg_complete, I2C_TIMEOUT);
+
+	i2c_int_disable(idev, int_mask);
+
+	if (WARN_ON(ret == 0)) {
+		dev_warn(idev->dev, "i2c transfer timed out\n");
+		/* Reset i2c controller and re-initialize */
+		writel(0x01, &idev->regs->soft_reset);
+		while (readl(&idev->regs->soft_reset) & 1)
+			cpu_relax();
+		axxia_i2c_init(idev);
+		return -ETIMEDOUT;
+	}
+
+	WARN_ON(readl(&idev->regs->mst_command) & 0x8);
+
+	dev_dbg(idev->dev, "transfer complete: %d %d %#x\n",
+		ret, completion_done(&idev->msg_complete), idev->msg_err);
+
+	if (likely(idev->msg_err == 0))
+		return 0;
+
+	return -EIO;
+}
+
+static int
+axxia_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
+{
+	struct axxia_i2c_dev *idev = i2c_get_adapdata(adap);
+	int i;
+	int ret = 0;
+
+	for (i = 0; ret == 0 && i < num; i++) {
+		int stop = (i == num-1);
+		ret = axxia_i2c_xfer_msg(idev, &msgs[i], stop);
+	}
+
+	return ret ?: i;
+}
+
+static u32
+axxia_i2c_func(struct i2c_adapter *adap)
+{
+	return I2C_FUNC_I2C | I2C_FUNC_10BIT_ADDR | I2C_FUNC_SMBUS_EMUL;
+
+}
+#else
 static int
 axxia_i2c_xfer_msg(struct axxia_i2c_dev *idev, struct i2c_msg *msg)
 {
@@ -476,6 +643,7 @@ axxia_i2c_func(struct i2c_adapter *adap)
 		I2C_FUNC_SMBUS_BLOCK_DATA);
 
 }
+#endif
 
 static const struct i2c_algorithm axxia_i2c_algo = {
 	.master_xfer	= axxia_i2c_xfer,
