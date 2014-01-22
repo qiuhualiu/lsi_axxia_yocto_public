a48df65a023494b37b38cb6a87d3ddf2e682bdb7
Michael Bringmann <michael.bringmann@lsi.com>
2014-01-22 17:03:23 -0600

    LSI AXM55xx: Various performance fixes for rapidio endpoint code.
    Signed-off-by: Michael Bringmann <michael.bringmann@lsi.com>

--- drivers/rapidio/rio.c
+++ drivers/rapidio/rio.c
@@ -414,7 +414,10 @@ int rio_release_outb_mbox(struct rio_mport *mport, int mbox)
 		mport->ops->close_outb_mbox(mport, mbox);
 
 		/* Release the mailbox resource */
-		return release_resource(mport->outb_msg[mbox].res);
+		if (mport->outb_msg[mbox].res)
+			return release_resource(mport->outb_msg[mbox].res);
+		else
+			return -ENOMEM;
 	} else
 		return -ENOSYS;
 }
@@ -1723,7 +1726,7 @@ static void rio_fixup_device(struct rio_dev *dev)
 {
 }
 
-static int __devinit rio_init(void)
+static int rio_init(void)
 {
 	struct rio_dev *dev = NULL;
 
@@ -1736,7 +1739,7 @@ static int __devinit rio_init(void)
  * @note No lock; Assuming this is used at boot time only,
  *       before start of user space
  */
-int __devinit rio_init_mports(void)
+int rio_init_mports(void)
 {
 	struct rio_mport *port;
 
